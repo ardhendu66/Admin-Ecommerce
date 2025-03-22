1cc49e9dd65d02c28da031b40044581cacf5e26d
ardhendu66 <140969694+ardhendu66@users.noreply.github.com>
2025-03-23 02:13:28 +0530

    Update create.ts

--- pages/api/upload/create.ts
+++ pages/api/upload/create.ts
@@ -28,8 +28,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                     return res.status(200).json({ message: "Brand already exist" });
                 }
 
-/**
-
               const updatedProduct = await Upload.findOneAndUpdate({name}, {
                     $push: {
                         brand: {
@@ -48,8 +46,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 }
 
                 return res.status(403).json({message: "Product creation failed"});
-
-**/
 
             }
 
