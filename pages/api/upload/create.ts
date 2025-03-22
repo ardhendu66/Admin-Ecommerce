81374fe9664955c7ea90e5effd756c136a805b36
ardhendu66 <140969694+ardhendu66@users.noreply.github.com>
2025-03-23 01:58:41 +0530

    API changes in Upload section

--- pages/api/upload/create.ts
+++ pages/api/upload/create.ts
@@ -48,9 +48,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 }
 
                 return res.status(403).json({message: "Product creation failed"});
-            }
 
 **/
+
+            }
 
 
             const uploadedData = await Upload.create({
